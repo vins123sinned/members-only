3aea3f187c4f0098bc62401bd62238daf44ee099
Vinson <vinsonhe2@gmail.com>
2025-10-21 18:24:44 -0700

    feat(sign-up): set up passport.js functions
    - Set up Passport.js' `use`, `serialize`, and `deserialize`
      functions to authenticate users
    - Add `getUserByUsername` and `getUserById` queries in
      `queries.js`

--- db/queries.js
+++ db/queries.js
@@ -1,5 +1,20 @@
 import { pool } from "./pool.js";
 
+const getUserByUsername = async (username) => {
+  const { rows } = await pool.query(
+    "SELECT * FROM users WHERE username = $1 LIMIT 1",
+    [username],
+  );
+  return rows[0];
+};
+
+const getUserById = async (id) => {
+  const { rows } = pool.query("SELECT * FROM users WHERE id = $1 LIMIT 1", [
+    id,
+  ]);
+  return rows[0];
+};
+
 const insertUser = async (first_name, last_name, username, password) => {
   await pool.query(
     "INSERT INTO users (first_name, last_name, username, password) VALUES ($1, $2, $3, $4)",
--- routes/authenticationRouter.js
+++ routes/authenticationRouter.js
@@ -1,5 +1,39 @@
 import { Router } from "express";
-import { getSignUp, postSignUp } from "../controllers/authenticationController.js";
+import {
+  getSignUp,
+  postSignUp,
+} from "../controllers/authenticationController.js";
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import { getUserById, getUserByUsername } from "../db/queries.js";
+
+const verify = async (username, password, done) => {
+  const user = await getUserByUsername(username);
+
+  try {
+    if (!user) return done(null, false, { message: "Incorrect username!" });
+    if (user.password !== password)
+      return done(null, false, { message: "Incorrect password" });
+    return done(null, user);
+  } catch (err) {
+    return done(err);
+  }
+};
+
+passport.use(new LocalStrategy(verify));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await getUserById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
 
 const authenticationRouter = Router();
 
