9425789e419545ec56c751425e62fa4be790c27a
Vinson <vinsonhe2@gmail.com>
2025-10-27 08:15:42 -0700

    feat(homepage): pass messages local to view
    Add `messages` local which takes the messages from the database, maps it
    to replace the author ids with their names, and then display it onto the
    view

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,14 +1,26 @@
+import { getAllMessages, getUserById } from "../db/queries.js";
 import { CustomNotFoundError } from "../errors/CustomNotFoundError.js";
 
-const getHomePage = (req, res) => {
+const getHomePage = async (req, res) => {
   // just to showcase error functionality
   const indexPage = true;
   if (!indexPage) {
     throw new CustomNotFoundError("Index page not found");
   }
 
+  const messages = await getAllMessages();
+  // return messages array with author's first name instead of id
+  const mappedMessages = await Promise.all(
+    messages.map(async (message) => {
+      const user = await getUserById(message.id);
+      message.author = user.first_name;
+      return message;
+    }),
+  );
+
   res.render("index", {
     title: "Homepage",
+    messages: mappedMessages,
   });
 };
 
