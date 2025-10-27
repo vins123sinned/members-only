1c67b39dcc8c3ef7e8d2d35b4d1cb707275fd5fe
Vinson <vinsonhe2@gmail.com>
2025-10-27 11:38:16 -0700

    feat(database): add admin column
    Update `users` table to add an `is_admin` column to allow for admin
    perks

--- db/populatedb.js
+++ db/populatedb.js
@@ -19,7 +19,8 @@ CREATE TABLE IF NOT EXISTS users (
   last_name VARCHAR ( 64 ),
   username VARCHAR ( 255 ) NOT NULL,
   password VARCHAR ( 255 ) NOT NULL,
-  is_member BOOLEAN DEFAULT FALSE
+  is_member BOOLEAN DEFAULT FALSE,
+  is_admin BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE IF NOT EXISTS messages (
