ec79efae4a7a73909750a7cd3ab82d4b4bd7d91a
Vinson <vinsonhe2@gmail.com>
2025-10-28 14:15:11 -0700

    feat(messages): add delete button for admins
    If the user is an admin, a delete button will appear that allows the
    user to delete a message along with a confirmation modal.
    
    - Show delete button and confirmation in `index.ejs`
    - Add delete confirmation modal functionality in `messageDelete.js`
    - Add styling to hide form in `styles.css`

---
+++ public/messageDelete.js
@@ -0,0 +1,21 @@
+const deleteButtons = document.querySelectorAll(".delete-button");
+const formCancelButton = document.querySelector(".form-cancel-button");
+const formDeleteButton = document.querySelector(".form-delete-button");
+const deleteConfirmationSection = document.querySelector(
+  ".delete-confirmation-section",
+);
+const deleteConfirmationForm = document.querySelector(
+  ".delete-confirmation-form",
+);
+
+deleteButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    deleteConfirmationSection.classList.remove("hidden");
+    deleteConfirmationForm.action = `messages/delete/${button.dataset.id}`;
+  });
+});
+
+formCancelButton.addEventListener("click", () => {
+  deleteConfirmationSection.classList.add("hidden");
+  deleteConfirmationForm.removeAttribute("action");
+});
