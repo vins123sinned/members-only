8c1821c43a78bf809dba445cf53f87cfb8a0d751
Vinson <vinsonhe2@gmail.com>
2025-10-28 14:28:33 -0700

    feat(messages): add message delete route

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   getMessageForm,
+  postMessageDelete,
   postMessageForm,
 } from "../controllers/messageController.js";
 
@@ -8,5 +9,6 @@ const messageRouter = Router();
 
 messageRouter.get("/create", getMessageForm);
 messageRouter.post("/create", postMessageForm);
+messageRouter.post("/delete/:messageId", postMessageDelete);
 
 export { messageRouter };
